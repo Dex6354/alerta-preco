3ac35ff9d9486eb3430952c05ee8ee2a3b791121
Dex6354 <dex.teste@proton.me>
2026-05-31 14:29:28 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -11,7 +11,7 @@ https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=0
 Cenoura, 10.00
 https://www.nagumo.com.br/categoria/departamentos/hortifruti/legumes/tuberculos/cenoura-13772.html
 
-Sorvete bombom, 50
+Sorvete bombom, 30
 https://www.loja.shibata.com.br/produto/11622/sorvete-bombom-jundia-pote-2l
 https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/sorvetes/sorvete-jundia-pote-2lt.-bombom-558617.html
 
@@ -19,7 +19,7 @@ Arroz Camil, 19
 https://www.loja.shibata.com.br/produto/12542/arroz-branco-camil-tipo-1-pacote-5kg
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/arroz/arroz-branco-tipo-1-camil-5kg-293303.html
 
-Leite integral, 5
+Leite integral, 4.90
 https://www.loja.shibata.com.br/produto/15500/leite-uht-integral-jussara-caixa-com-tampa-1l
 https://www.loja.shibata.com.br/produto/12987/leite-longa-vida-batavo-integral-caixa-com-tampa-1l
 https://www.loja.shibata.com.br/produto/11158/leite-uht-com-tampa-integral-parmalat-caixa-1l
