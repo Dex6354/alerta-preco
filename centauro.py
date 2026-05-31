86bc4643e497eb7fedca0c4abdd9dc6ed543387c
Dex6354 <dex.teste@proton.me>
2026-05-31 19:27:04 -0300

    Update centauro.py

--- centauro.py
+++ centauro.py
@@ -96,8 +96,7 @@ def enviar_telegram_foto(token, chat_id, foto_url, caption, nome_arquivo):
         if not img_resp.ok:
             raise Exception(f"Erro ao baixar imagem: {img_resp.status_code}")
         
-        filename_limpo = re.sub(r'[\\/*?:"<>|]', "", nome_arquivo).strip()
-        filename = "item.jpg"
+        filename = "..jpg"
 
         url = f"https://api.telegram.org/bot{token}/sendDocument"
         data = {"chat_id": chat_id, "caption": caption, "parse_mode": "HTML"}
