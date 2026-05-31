b3fbeda4b77a3da7bec6ba1acc648a4ccc0fc2c5
Dex6354 <dex.teste@proton.me>
2026-05-31 20:53:06 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -25,7 +25,7 @@ https://www.loja.shibata.com.br/produto/12987/leite-longa-vida-batavo-integral-c
 https://www.loja.shibata.com.br/produto/11158/leite-uht-com-tampa-integral-parmalat-caixa-1l
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-uht-integral-quat%C3%A1-1l-312738.html
 
-Peito de frango, 25
+Peito de frango, 21
 https://www.loja.shibata.com.br/produto/16661/peito-de-frango-sem-pele-e-sem-osso-bandeja-com-aprox-12kg
 https://www.nagumo.com.br/categoria/departamentos/acougue/ave/ave-refrigerada/peito-de-frango-sem-osso-resfriado-206396.html
 
