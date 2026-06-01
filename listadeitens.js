1a3d2ade809e683e0869a27c75d70742a6bb94c9
Dex6354 <dex.teste@proton.me>
2026-06-01 07:13:46 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -1,36 +1,10 @@
 Tênis, 300.00
 https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=31
-
-Regata Oxer, 80.00
-https://www.centauro.com.br/regata-oxer-regata-respirabilidade-mas-984829.html?cor=83
-
-Conjunto oxer, 150.00
-https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=05
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=02
 
-Cenoura, 9.00
-https://www.nagumo.com.br/categoria/departamentos/hortifruti/legumes/tuberculos/cenoura-13772.html
-
-Sorvete bombom, 50
-https://www.loja.shibata.com.br/produto/11622/sorvete-bombom-jundia-pote-2l
-https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/sorvetes/sorvete-jundia-pote-2lt.-bombom-558617.html
-
-Arroz Camil, 19
-https://www.loja.shibata.com.br/produto/12542/arroz-branco-camil-tipo-1-pacote-5kg
-https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/arroz/arroz-branco-tipo-1-camil-5kg-293303.html
-
-Leite integral, 4.88
+Leite integral, 6
 https://www.loja.shibata.com.br/produto/15500/leite-uht-integral-jussara-caixa-com-tampa-1l
 https://www.loja.shibata.com.br/produto/12987/leite-longa-vida-batavo-integral-caixa-com-tampa-1l
-https://www.loja.shibata.com.br/produto/11158/leite-uht-com-tampa-integral-parmalat-caixa-1l
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-uht-integral-quat%C3%A1-1l-312738.html
-
-Peito de frango, 25
-https://www.loja.shibata.com.br/produto/16661/peito-de-frango-sem-pele-e-sem-osso-bandeja-com-aprox-12kg
-https://www.nagumo.com.br/categoria/departamentos/acougue/ave/ave-refrigerada/peito-de-frango-sem-osso-resfriado-206396.html
-
-Ovo, 10
-https://www.loja.shibata.com.br/produto/15362/ovo-mantiqueira-extra-branco-20-unidades
-https://www.loja.shibata.com.br/produto/89076/ovos-brancos-da-granja-shida-extra-pvc-20-unidades
-https://www.nagumo.com.br/categoria/departamentos/hortifruti/granjeiros/ovos/ovos-branco-extra-grande-20un-274901.html
+https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-parmalat-semi-desnatado-1l-532426.html
 
