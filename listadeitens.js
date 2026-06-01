933b8a8a2a5cc7e88bbf07e779ce1fed9d08bb0b
Dex6354 <dex.teste@proton.me>
2026-06-01 07:14:10 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -3,8 +3,6 @@ https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=31
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=02
 
 Leite integral, 6
-https://www.loja.shibata.com.br/produto/15500/leite-uht-integral-jussara-caixa-com-tampa-1l
-https://www.loja.shibata.com.br/produto/12987/leite-longa-vida-batavo-integral-caixa-com-tampa-1l
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-uht-integral-quat%C3%A1-1l-312738.html
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-parmalat-semi-desnatado-1l-532426.html
 
