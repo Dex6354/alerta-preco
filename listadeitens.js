e1773bab8ec6f3470784fd92e538d1d5bf457a6d
Dex6354 <dex.teste@proton.me>
2026-06-01 07:46:12 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -1,9 +1,6 @@
 Tênis, 300.00
 https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=31
-https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=32
-https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=33
-https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=01
-https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=02
+
 
 Leite integral, 4
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/leite-uht/leite-uht-integral-quat%C3%A1-1l-312738.html
