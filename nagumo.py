2dbf41ed79ec2e0cb0ae600d17d068be704f5719
Dex6354 <dex.teste@proton.me>
2026-06-01 21:55:09 -0300

    Update nagumo.py

--- nagumo.py
+++ nagumo.py
@@ -219,9 +219,8 @@ def main():
     if todos_atingidos:
         for p in todos_atingidos:
             caption = (
-                f"<b>━━━━━━━━━━━━━━━━━━━━━</b>\n"
                 f"🛒 <b>NAGUMO</b>: <a href='{p['url']}'>{p['nome']}</a>\n"
-                f"💰 <b>R$ {p['preco']:.2f}</b> | 🎯 <b>R$ {p['alvo']:.2f}</b>\n"
+                f"💰 <b>R$ {p['preco']:.2f}</b> | 🎯 <b>R$ {p['alvo']:.2f}</b> ({p['tipo']})\n"
                 f"<b>━━━━━━━━━━━━━━━━━━━━━</b>\n"
             )
             if p["imagem_url"]:
