d6b8872f11518f8edc222c206810f074a171d1fc
Dex6354 <dex.teste@proton.me>
2026-06-02 20:07:02 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -9,7 +9,7 @@ Conjunto oxer, 120.00
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=05
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=02
 
-Cenoura, 15.00
+Cenoura, 11.00
 https://www.nagumo.com.br/categoria/departamentos/hortifruti/legumes/tuberculos/cenoura-13772.html
 
 Sorvete bombom, 30
