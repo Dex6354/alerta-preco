d4337af5212f73be04197d3787596d0f3a13967b
Dex6354 <dex.teste@proton.me>
2026-06-02 19:59:07 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -1,7 +1,8 @@
-Tênis, 300.00
+Tênis Nike, 200.00
 https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=31
+https://www.centauro.com.br/tenis-nike-air-winfl-11-masculino-988389.html?cor=31
 
-Regata Oxer, 80.00
+Regata Oxer, 50.00
 https://www.centauro.com.br/regata-oxer-regata-respirabilidade-mas-984829.html?cor=83
 
 Conjunto oxer, 150.00
@@ -11,7 +12,7 @@ https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=0
 Cenoura, 15.00
 https://www.nagumo.com.br/categoria/departamentos/hortifruti/legumes/tuberculos/cenoura-13772.html
 
-Sorvete bombom, 50
+Sorvete bombom, 30
 https://www.loja.shibata.com.br/produto/11622/sorvete-bombom-jundia-pote-2l
 https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/sorvetes/sorvete-jundia-pote-2lt.-bombom-558617.html
 
