81883d4df17c1a8e244890866bb3ee1529f122b0
Dex6354 <dex.teste@proton.me>
2026-06-03 11:23:03 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -14,25 +14,22 @@ https://www.centauro.com.br/camiseta-regata-penalty-treino-essencial-basico-masc
 https://www.centauro.com.br/regata-machao-diadora-big-logo-masculina-m17s69-mktp.html?cor=02
 https://www.centauro.com.br/regata-machao-topper-classic-masculina-m16rj4-mktp.html?cor=02
 
-Conjunto oxer, 120.00
+Conjunto oxer, 112.00
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=05
 https://www.centauro.com.br/conjunto-de-agasalho-oxer-replayer-981478.html?cor=02
 
-Cenoura, 11.00
+Cenoura, 10.97
 https://www.nagumo.com.br/categoria/departamentos/hortifruti/legumes/tuberculos/cenoura-13772.html
 
 Sorvete bombom, 30
 https://www.loja.shibata.com.br/produto/11622/sorvete-bombom-jundia-pote-2l
 https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/sorvetes/sorvete-jundia-pote-2lt.-bombom-558617.html
 
-Arroz Camil, 19
+Arroz Camil, 19.98
 https://www.loja.shibata.com.br/produto/12542/arroz-branco-camil-tipo-1-pacote-5kg
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/arroz/arroz-branco-tipo-1-camil-5kg-293303.html
 
-Teste, 18
-https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/vegetal-congelado/batata-palito-congelada-nagumo-1%2C05kg-259978.html
-
-Leite integral, 4.88
+Leite integral, 4.89
 https://www.loja.shibata.com.br/produto/15500/leite-uht-integral-jussara-caixa-com-tampa-1l
 https://www.loja.shibata.com.br/produto/12987/leite-longa-vida-batavo-integral-caixa-com-tampa-1l
 https://www.loja.shibata.com.br/produto/11158/leite-uht-com-tampa-integral-parmalat-caixa-1l
