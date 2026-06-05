270aaecbe46ebbb798b8eef8d43303955314f66f
Dex6354 <dex.teste@proton.me>
2026-06-05 16:35:25 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -1,7 +1,6 @@
 Tênis Nike, 300.00
 https://www.centauro.com.br/tenis-masculino-nike-revolution-8-995996.html?cor=31
 https://www.centauro.com.br/tenis-nike-air-winfl-11-masculino-988389.html?cor=31
-https://www.centauro.com.br/tenis-nike-air-winfl-11-masculino-9883k9.html?cor=31
 
 Blusa, 500
 https://www.centauro.com.br/conjunto-de-agasalho-masculino-asics-com-capuz-interlock-fechado-976758.html?cor=02
@@ -32,8 +31,6 @@ https://www.nagumo.com.br/categoria/departamentos/frios-e-laticinios/congelados/
 Arroz Camil, 20
 https://www.loja.shibata.com.br/produto/12542/arroz-branco-camil-tipo-1-pacote-5kg
 https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/arroz/arroz-branco-tipo-1-camil-5kg-293303.html
-https://www.nagumo.com.br/categoria/departamentos/basicos-e-matinais/commodities/arroz/arroz-branco-tipo-1-camkl-5kg-293399.html
-https://www.loja.shibata.com.br/produto/89999/arroz-branco-camkl-tipo-1-pacote-5kg
 
 Leite integral, 4.68
 https://www.loja.shibata.com.br/produto/15500/leite-uht-integral-jussara-caixa-com-tampa-1l
