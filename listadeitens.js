ee77023ae144caf1f4d66cec0487cfca68def5f6
Dex6354 <dex.teste@proton.me>
2026-06-21 21:56:12 -0300

    Update listadeitens.js

--- listadeitens.js
+++ listadeitens.js
@@ -6,6 +6,9 @@ Cueca, 54
 https://www.centauro.com.br/kit-de-cuecas-boxer-sem-costura-puma-com-2-unidades-adulto-954445.html?cor=29
 https://www.centauro.com.br/kit-de-2-cuecas-lupo-boxer-sem-costura-988571.html?cor=63
 
+Chinelo, 32
+https://www.centauro.com.br/chinelo-masculino-havaianas-track-waves-995894.html?cor=02
+
 Bermuda, 30
 https://www.centauro.com.br/bermuda-masculina-oxer-training-7-tecido-plano-981429.html?cor=02
 
